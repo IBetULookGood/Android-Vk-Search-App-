605438089a3be555c0d908dd9de85a0694528bec
IBetULookGood <alekceychick.tim@gmail.com>
2023-10-09 16:39:58 +0300

    handle a recyclerView

--- app/src/main/java/com/example/vk_app/MainActivity.java
+++ app/src/main/java/com/example/vk_app/MainActivity.java
@@ -1,107 +1,29 @@
 package com.example.vk_app;
 
 import android.annotation.SuppressLint;
-import android.os.AsyncTask;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 
-import java.io.IOException;
-import java.net.URL;
-
-import static com.example.vk_app.utils.NetworkUtils.generateUrl;
-import static com.example.vk_app.utils.NetworkUtils.getResponseFromURL;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-
-public class MainActivity extends AppCompatActivity {
-
-    private EditText searchField;
-    private Button searchButton;
-    private TextView result;
-    private TextView errorMessage;
-    private ProgressBar loadingIndicator;
-
-    private void showResultTextView(){
-        result.setVisibility(View.VISIBLE);
-        errorMessage.setVisibility(View.INVISIBLE);
-    }
-
-    private void showErrorTextView(){
-        result.setVisibility(View.INVISIBLE);
-        errorMessage.setVisibility(View.VISIBLE);
-    }
-
-
-    class VKQueryTask extends AsyncTask<URL, Void, String>{
-
-        @Override
-        protected void onPreExecute(){
-            loadingIndicator.setVisibility(View.VISIBLE);
-        }
-
-        @Override
-        protected String doInBackground(URL... urls) {
-            String response = null;
-            try{
-                response = getResponseFromURL(urls[0]);
-            } catch (IOException e){
-                e.printStackTrace();
-            }
-            return response;
-        }
-
-        @Override
-        protected void onPostExecute(String response){
-            String firstName = null;
-            String lastName = null;
-
-            if(response != null && !response.equals("")) {
-                try {
-                    JSONObject jsonResponse = new JSONObject(response);
-                    JSONArray jsonArray = jsonResponse.getJSONArray("response");
-                    JSONObject userInfo = jsonArray.getJSONObject(0);
-
-                    firstName = userInfo.getString("first_name");
-                    lastName = userInfo.getString("last_name");
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-                String resultingString = "Имя: " + firstName + "\n" + "Фамилия: " + lastName;
-                result.setText(resultingString);
-
-                showResultTextView();
-            }else{
-                showErrorTextView();
-            }
-            loadingIndicator.setVisibility(View.INVISIBLE);
-        }
-    }
-
+public class MainActivity extends AppCompatActivity{
+    private RecyclerView numberList;
+    private NumbersAdapter numbersAdapter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        searchField = findViewById(R.id.et_search_field);
-        searchButton = findViewById(R.id.b_search_vk);
-        result = findViewById(R.id.tv_result);
-        errorMessage = findViewById(R.id.tv_error_message);
-        loadingIndicator = findViewById(R.id.pb_loading_indicator);
+        numberList = findViewById(R.id.rv_numbers);
 
-        View.OnClickListener onClickListener = view -> {
-            URL generatedURL = generateUrl(searchField.getText().toString());
-            new VKQueryTask().execute(generatedURL);
-        };
-        searchButton.setOnClickListener(onClickListener);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        numberList.setLayoutManager(layoutManager);
+
+        numberList.setHasFixedSize(true);
+
+        numbersAdapter = new NumbersAdapter(100);
+        numberList.setAdapter(numbersAdapter);
     }
 }
