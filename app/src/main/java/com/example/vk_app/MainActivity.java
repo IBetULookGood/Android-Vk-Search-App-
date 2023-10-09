ad37c9dcb4f121a7083b1fbd0b5e87102af5cf6f
IBetULookGood <alekceychick.tim@gmail.com>
2023-10-09 21:55:20 +0300

    handle intents

--- app/src/main/java/com/example/vk_app/MainActivity.java
+++ app/src/main/java/com/example/vk_app/MainActivity.java
@@ -5,25 +5,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity{
-    private RecyclerView numberList;
-    private NumbersAdapter numbersAdapter;
+
+    private EditText textEntry;
+    private Button changeActivityButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        numberList = findViewById(R.id.rv_numbers);
+        changeActivityButton = findViewById(R.id.change_activity);
+        textEntry = findViewById(R.id.et_text_entry);
 
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        numberList.setLayoutManager(layoutManager);
+        changeActivityButton.setOnClickListener(view -> {
+            String textEntered = textEntry.getText().toString();
+            Context context = MainActivity.this;
 
-        numberList.setHasFixedSize(true);
-
-        numbersAdapter = new NumbersAdapter(100, this);
-        numberList.setAdapter(numbersAdapter);
+            Class destinationActivity = ChildActivity.class;
+            Intent childActivityIntent = new Intent(context, destinationActivity);
+            childActivityIntent.putExtra(Intent.EXTRA_TEXT, textEntered);
+            startActivity(childActivityIntent);
+        });
     }
 }
