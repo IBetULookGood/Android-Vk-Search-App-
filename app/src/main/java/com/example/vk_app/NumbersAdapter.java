5e9322abbdbb09404e92c891867b1aada3c59b84
IBetULookGood <alekceychick.tim@gmail.com>
2023-10-09 16:57:04 +0300

    add onClickListener

--- app/src/main/java/com/example/vk_app/NumbersAdapter.java
+++ app/src/main/java/com/example/vk_app/NumbersAdapter.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -14,11 +15,14 @@ public class NumbersAdapter extends RecyclerView.Adapter<NumbersAdapter.NumberVi
 
     private static int viewHolderCount;
     private int numberItems;
+    private Context parent;
 
-    public NumbersAdapter(int numberOfItems){
+    public NumbersAdapter(int numberOfItems, Context parent){
         numberItems = numberOfItems;
         viewHolderCount = 0;
+        this.parent = parent;
     }
+
     @SuppressLint("SetTextI18n")
     @NonNull
     @Override
@@ -57,6 +61,13 @@ public class NumbersAdapter extends RecyclerView.Adapter<NumbersAdapter.NumberVi
 
             listItemNumberView = itemView.findViewById(R.id.tv_number_item);
             viewHolderIndex = itemView.findViewById(R.id.tv_view_holder_number);
+
+            itemView.setOnClickListener(view -> {
+                int positionIndex = getAdapterPosition();
+
+                Toast toast = Toast.makeText(parent, "Element " + positionIndex + " was clicked!", Toast.LENGTH_SHORT);
+                toast.show();
+            });
         }
         void bind(int listIndex){
             listItemNumberView.setText(String.valueOf(listIndex));
