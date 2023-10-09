e9fb5174cd291283e37a168215b74b846e29fd67
IBetULookGood <alekceychick.tim@gmail.com>
2023-10-09 22:35:39 +0300

    add web and map handling

--- app/src/main/java/com/example/vk_app/MainActivity.java
+++ app/src/main/java/com/example/vk_app/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,25 +16,31 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity{
 
-    private EditText textEntry;
-    private Button changeActivityButton;
-
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        changeActivityButton = findViewById(R.id.change_activity);
-        textEntry = findViewById(R.id.et_text_entry);
+    public void onClickOpenWebpageButton(View view){
+        openWebPage("https://www.google.com");
+    }
+    public void onClickOpenMapButton(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        Uri addressUri = Uri.parse("geo:0,0")
+                .buildUpon().appendQueryParameter("q","Тверская улица, 1")
+                .build();
+        intent.setData(addressUri);
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        }
+    }
 
-        changeActivityButton.setOnClickListener(view -> {
-            String textEntered = textEntry.getText().toString();
-            Context context = MainActivity.this;
-
-            Class destinationActivity = ChildActivity.class;
-            Intent childActivityIntent = new Intent(context, destinationActivity);
-            childActivityIntent.putExtra(Intent.EXTRA_TEXT, textEntered);
-            startActivity(childActivityIntent);
-        });
+    private void openWebPage(String url){
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        if(intent.resolveActivity(getPackageManager())!=null){
+            startActivity(intent);
+        }
     }
 }
